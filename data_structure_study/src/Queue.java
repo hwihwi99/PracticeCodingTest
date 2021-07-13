2c37dd456a590814d7fcab3c137fe7ca15b710de
hwihwi99 <chamgrace@naver.com>
2021-07-13 23:42:43 +0900

    Stack&Queue

--- data_structure_study/src/Queue.java
+++ data_structure_study/src/Queue.java
@@ -1,2 +1,28 @@
 public class Queue {
+    private Object [] arr;
+    private int head;
+    private int tail;
+
+    public Queue(int size){
+        arr = new Object[size];
+        head = 0;
+        tail = 0;
+    }
+
+    public int size(){
+        return tail - head;
+    }
+
+    public boolean isEmpty(){
+        return head == tail;
+    }
+
+    public void enqueue(Object obj){
+        arr[tail++] = obj;
+    }
+
+    public Object dequeue(){
+        return arr[head++];
+    }
+
 }
--- data_structure_study/src/Stack.java
+++ data_structure_study/src/Stack.java
@@ -1,2 +1,33 @@
 public class Stack {
+    private Object [] arr;
+    private int top;
+
+    public Stack(int size){
+        arr = new Object[size];
+        top = 0;
+    }
+
+    public boolean isEmpty(){
+        return top == 0;
+    }
+
+    public boolean isFull(){
+        return top == arr.length-1;
+    }
+
+    public void push(Object obj){
+        if(!isFull())
+            arr[top++] = obj;
+        else
+            System.out.println("Stack is full");
+    }
+
+    public Object pop(){
+        if(!isEmpty())
+            return arr[--top];
+        else {
+            System.out.println("Stack is empty");
+            return null;
+        }
+    }
 }
