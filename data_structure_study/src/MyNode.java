942770e1244ad7ea4d188c9e32d16383780df241
hwihwi99 <chamgrace@naver.com>
2021-08-04 16:28:07 +0900

    add file

---
+++ data_structure_study/src/MyNode.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class MyNode {
+    private MyNode parent;
+    private ArrayList children;
+    private int cost;
+    private Object element;
+
+    public MyNode(MyNode parent, int cost, Object element) {
+        this.parent = parent;
+        this.cost = cost;
+        this.element = element;
+        this.children = new ArrayList();
+    }
+
+    public MyNode getParent() {
+        return parent;
+    }
+
+    public void setParent(MyNode parent) {
+        this.parent = parent;
+    }
+
+    public ArrayList getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList children) {
+        this.children = children;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public Object getElement() {
+        return element;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+}
---
+++ data_structure_study/src/MyTree.java
@@ -0,0 +1,21 @@
+
+public class MyTree {
+    private MyNode root;
+    public MyTree() {
+        this.root = null;
+    }
+    public MyTree(int cost, Object element) {
+        this.root = new MyNode(null, cost, element);
+    }
+
+    public MyNode root() {
+        return this.root;
+    }
+
+    public MyNode addChild(MyNode v, int cost, Object element) {
+        MyNode newNode = new MyNode(v, cost, element);
+        v.getChildren().add(newNode);
+        return newNode;
+    }
+
+}
