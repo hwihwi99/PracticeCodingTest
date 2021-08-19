ba56a498bbb0d45533c9f334498bafb160e85132
hwihwi99 <chamgrace@naver.com>
2021-08-19 21:50:32 +0900

    tree_part2_reset

--- data_structure_study/src/MyBinNode.java
+++ data_structure_study/src/MyBinNode.java
@@ -1,26 +1,3 @@
 public class MyBinNode extends MyNode {
-    MyBinNode(){
-        super();
-    }
 
-    MyBinNode(Object e){
-        super(e);
-    }
-
-    // 왼쪽 자식 : 0, 오른쪽 자식 : 1
-
-    public MyBinNode left(){
-        return (MyBinNode) super.children().get(0);
-    }
-
-    public MyBinNode right(){
-        return (MyBinNode) super.children().get(1);
-    }
-
-    public void setLeft (MyBinNode v){
-        super.children().set(0,v);
-    }
-    public void setRight (MyBinNode v){
-        super.children().set(1,v);
-    }
 }
