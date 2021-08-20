65520be282fbbd6e67d9abe98a0f1b0d86112cab
hwihwi99 <chamgrace@naver.com>
2021-08-20 23:37:04 +0900

    MyNodeTree

--- data_structure_study/src/MyBinNode.java
+++ data_structure_study/src/MyBinNode.java
@@ -1,3 +1,29 @@
 public class MyBinNode extends MyNode {
+    public MyBinNode(){
+        super();
+    }
 
+    public MyBinNode(Object e){
+        super(e);
+    }
+
+    // 2진트리.. -> 왼쪽은 0번, 오른쪽은 1번 인덱스!
+
+    // 그 궁금한게... 그 ArrayList = new ArrayList 이런거 안해주고 어케 사용해...??
+
+    public MyBinNode left(){
+        return (MyBinNode) super.children().get(0);
+    }
+
+    public MyBinNode right(){
+        return (MyBinNode) super.children().get(1);
+    }
+
+    public void setLeft(MyBinNode v){
+        super.children().set(0,v);
+    }
+
+    public void setRight(MyBinNode v){
+        super.children().set(1,v);
+    }
 }
--- data_structure_study/src/MyBinTree.java
+++ data_structure_study/src/MyBinTree.java
@@ -1,4 +1,20 @@
 import java.util.ArrayList;
 public class MyBinTree extends MyTree{
+    // 예외처리 (Exception) : 오류가 발생했을 때 그 처리를 예외에게 맡기는 것
+    public class TwoChildrenException extends Exception{
+        // 자식이 이미 두명이면..
+        // 즉 자식이 있는데 그 노드를 지우려고 한다!
+        TwoChildrenException(String msg){
+            super(msg);
+        }
+    }
 
+    public class NotExternalException extends Exception{
+        // 해당 노드가 종단 노드가 아니라면.. 오류!
+        // 즉 자식 두개를 붙이려고 하는데 이미 자식이 있는 노드면 오류처리를 내자@
+    }
+    public MyBinTree(){
+        super();
+    }
+    public
 }
