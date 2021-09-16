b4cc8abd3bbbb3c93b7fe6bd646d7619a6a6ec10
hwihwi99 <chamgrace@naver.com>
2021-09-16 23:58:56 +0900

    myHashTable_1

--- data_structure_study/src/MyHashTable.java
+++ data_structure_study/src/MyHashTable.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
 
 public class MyHashTable {
-    private ArrayList<ArrayList<StudentInfo>> bucketArray;
+    private ArrayList bucketArray;
     private int bucketCapacity;
     private int totalCnt;
 
-    MyHashTable(int initialCapacity){
-        bucketArray = new ArrayList<>();
+    public MyHashTable(int initialCapacity){
+        bucketArray = new ArrayList();
         bucketCapacity = initialCapacity;
         totalCnt = 0;
-        for(int i = 0;i<bucketCapacity;i++){
+        for(int i = 0; i<bucketCapacity;i++){
             bucketArray.add(null);
         }
     }
@@ -17,27 +17,25 @@ public class MyHashTable {
     private int hashFunc(String k){
         int h = 0;
         int a = 33;
-
-        for(int i = 0;i<k.length();i++){
+        for(int i = 0; i<k.length();i++){
             h = (h<<5) | (h>>>27);
-            h+= (int)(k.charAt(i) * Math.pow(a,k.length()-i-1));
+            h = (int)(k.charAt(i)*Math.pow(a,k.length()-i-1));
         }
-        h %= this.bucketCapacity;
+        h %= bucketCapacity;
         return h;
     }
 
     public int size(){
-        return totalCnt;
+        return this.totalCnt;
     }
 
-    public String get (String k){
+    public String get(String k){
         int key = hashFunc(k);
-        ArrayList temp = (ArrayList) this.bucketArray.get(key);
-
+        ArrayList temp = (ArrayList) bucketArray.get(key);
         if(temp == null){
-            System.out.println("This key is not exist");
+            System.out.println("this key is not exist");
         }else{
-            for(int i = 0;i<temp.size();i++){
+            for(int i = 0; i<temp.size();i++){
                 StudentInfo info = (StudentInfo) temp.get(i);
                 if(k.equals(info.getStudentID())){
                     return info.getStudentName();
@@ -47,38 +45,60 @@ public class MyHashTable {
         return null;
     }
 
-    public String put(String k, String v){
-        StudentInfo info = new StudentInfo(k,v);
+    public String put (String k, String v) {
+        StudentInfo studentInfo = new StudentInfo(k, v);
         int key = hashFunc(k);
-        ArrayList<StudentInfo> temp = (ArrayList) bucketArray.get(key);
+        ArrayList temp = (ArrayList) bucketArray.get(key);
 
-        // 기존에 아무것도 없었다면
-        if(temp == null){
-            ArrayList <StudentInfo> newList = new ArrayList<StudentInfo>();
-            newList.add(info);
-            this.bucketArray.set(key,newList);
-            totalCnt ++;
+        if (temp == null) {
+            ArrayList newList = new ArrayList();
+            newList.add(studentInfo);
+            this.bucketArray.set(key, newList);
+            totalCnt++;
             return null;
         }
-
-        for(int i = 0; i< temp.size();i++){
-            StudentInfo tempInfo = (StudentInfo) temp.get(i);
-            if(k.equals(tempInfo.getStudentID())){
-                String result = tempInfo.getStudentName();
-                tempInfo.setStudentName(v);
-                return result;
-            }
-            if(i == temp.size()-1){
-                temp.add(info);
-                totalCnt++;
+        for (int i = 0; i < temp.size(); i++) {
+            StudentInfo info = (StudentInfo) temp.get(i);
+            if (k.equals(info.getStudentID())) {
+                String returnData = info.getStudentName();
+                temp.set(i, studentInfo);
+                return returnData;
             }
         }
+        temp.add(studentInfo);
+        totalCnt++;
+        return null;
+    }
+
+    public String remove(String k){
+        int key = hashFunc(k);
+        ArrayList temp = (ArrayList) bucketArray.get(key);
+        if(temp == null){
+            System.out.println("Key cannot find");
+            return null;
+        }
+        for(int i = 0;i< temp.size();i++){
+            StudentInfo info = (StudentInfo) temp.get(i);
+            if(k.equals(info.getStudentID())){
+                String returnData = info.getStudentName();
+                temp.remove(i);
+                totalCnt --;
+                return returnData;
+            }
+        }
+        System.out.println("Key cannot find");
         return null;
     }
 
     public static void main(String[] args) {
         MyHashTable hashTable = new MyHashTable(13);
-        hashTable.put("201810612","이휘정");
+        hashTable.put("123","A");
+        hashTable.put("34235","B");
+        hashTable.put("dfssdf","C");
+        hashTable.put("dsfs","D");
+        System.out.println(hashTable.get("123"));
+        System.out.println(hashTable.remove("123"));
+        System.out.println(hashTable.get("DSFS"));
         System.out.println(hashTable.bucketArray);
     }
 }
