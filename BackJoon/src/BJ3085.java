33377ab493e766bf716359046acbfe410531574f
hwihwi99 <chamgrace@naver.com>
2021-10-23 00:06:19 +0900

    bj3085

--- BackJoon/src/BJ3085.java
+++ BackJoon/src/BJ3085.java
@@ -5,37 +5,39 @@ import java.io.InputStreamReader;
 public class BJ3085 {
 
     public static int check(char [][] candy){
-       int ans = 0;
+       int ans = 1;
         int length = candy.length;
 
         int temp;
 
        for(int i = 0; i<length;i++){
            temp = 1;
-           for(int j = 0; j<length-1; j++){
-               if(candy[i][j] == candy[i][j+1]){
+           for(int j = 1; j<length; j++){
+               if(candy[i][j] == candy[i][j-1]){
                    temp++;
                }else{
                    //System.out.println(i+" "+j+" => "+temp+"개로 추정중");
-                   ans = Math.max(ans,temp);
                    temp = 1;
                }
+               if(ans<temp){
+                   ans = temp;
+               }
+           }
 
+           temp = 1;
+           for(int j = 1; j<length; j++){
+               if(candy[j][i] == candy[j-1][i]){
+                   temp++;
+               }else{
+                   //System.out.println(i+" "+j+" => "+temp+"개로 추정중");
+                   temp = 1;
+               }
+               if(ans<temp){
+                   ans = temp;
+               }
            }
        }
 
-        for(int i = 0; i<length-1;i++){
-            temp = 1;
-            for(int j = 0; j<length; j++){
-                if(candy[i][j] == candy[i+1][j]){
-                    temp++;
-                }else {
-                    //System.out.println(i + " " + j + " => " + temp + "개로 추정중");
-                    ans = Math.max(ans, temp);
-                    temp = 1;
-                }
-            }
-        }
        return ans;
     }
 
@@ -58,7 +60,7 @@ public class BJ3085 {
                 temp = candy[i][j];
                 candy[i][j] = candy[i][j+1];
                 candy[i][j+1] = temp;
-                System.out.println("한번 돌때마다"+i+" "+j+" => "+check(candy));
+                //System.out.println("한번 돌때마다"+i+" "+j+" => "+check(candy));
                 ans = Math.max(ans,check(candy));
 
                 temp = candy[i][j];
@@ -66,14 +68,14 @@ public class BJ3085 {
                 candy[i][j+1] = temp;
             }
         }
-        System.out.println("세로세로시작");
+        //System.out.println("세로세로시작");
         for(int i = 0;i<N-1;i++){
             for(int j = 0; j<N; j++){
                 //좌우로 바꾸면서 확인
                 temp = candy[i][j];
                 candy[i][j] = candy[i+1][j];
                 candy[i+1][j] = temp;
-                System.out.println("한번 돌때마다"+i+" "+j+" => "+check(candy));
+                //System.out.println("한번 돌때마다"+i+" "+j+" => "+check(candy));
                 ans = Math.max(ans,check(candy));
 
                 temp = candy[i][j];
