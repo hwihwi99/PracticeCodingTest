9419c1621d31f10c4672e19735b628d9a2adf725
hwihwi99 <chamgrace@naver.com>
2022-01-18 08:40:23 +0900

    bj 11399

--- BackJoon/src/BJ11399.java
+++ BackJoon/src/BJ11399.java
@@ -1,26 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class BJ11399 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        int []input = new int [num];
-        int []dp = new int [num];
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        for(int i = 0;i<num;i++){
-            input[i] = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        ArrayList<Integer> inputArray = new ArrayList<>();
+        for(int i = 0; i<N; i++) {
+            inputArray.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(input);
-        dp[0] = input[0];
-        int sum = dp[0];
-        for(int i = 1;i<num;i++) {
-            dp[i] = dp[i-1]+input[i];
-            sum += dp[i];
+
+        Collections.sort(inputArray);
+
+        int answer = 0;
+        int beforeSum = 0;
+        for(int i = 0; i<N; i++) {
+            beforeSum += inputArray.get(i);
+            answer += beforeSum;
         }
-        System.out.println(sum);
+
+        System.out.println(answer);
     }
 }
