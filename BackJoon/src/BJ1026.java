c874ae83439ff17b5915c0ad36033efa6c9bb495
hwihwi99 <chamgrace@naver.com>
2022-01-18 11:17:14 +0900

    BJ1026 주석 및 설명 완료

--- BackJoon/src/BJ1026.java
+++ BackJoon/src/BJ1026.java
@@ -5,6 +5,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
+/**
+ * 문제를 푼 방식
+ * A배열은 오름차순, B배열은 내림차순처리를 하고
+ * S = A[0] × B[0] + ... + A[N-1] × B[N-1]이와 같이 문제에서 주어진대로
+ * 인덱스가 같은 배열값끼리 곱해서 다 더해줍니다.
+ * 이러면 최소값이 나오게 됩니다.
+ *
+ * B는 재배열하면 안된다고 문제에 나와있지만 사실 이 문제는 B를 재배열하는지 안하는지 알 방법이 없습니다.
+ * 그래서 전 내림차순으로 정렬해주고 풀었더니 정답이였습니다.
+ *
+ * 새로 알게 된 사실
+ * 딱히 없습니다.
+ * */
 public class BJ1026 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
