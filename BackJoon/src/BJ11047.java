8ba5b0d6bad530019760c8f15edfa31ba2e1b7c5
hwihwi99 <chamgrace@naver.com>
2022-01-18 11:12:31 +0900

    BJ11047 주석 및 설명 완료

--- BackJoon/src/BJ11047.java
+++ BackJoon/src/BJ11047.java
@@ -5,6 +5,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
+/**
+ * 문제를 푼 방식
+ * 앞서 1,2주차에 풀었던 문제 중 유사한 것들이 있었습니다.
+ * 우선 동전의 가치가 큰 것부터 갯수를 줄여나가면 됩니다.
+ * 이 때 가장 작은 동전의 가치는 무조건 1이므로 동전이 나눠지지 않을 걱정은 하지 않아도 됩니다.
+ *
+ * 유사문제가 많았으므로 더 자세한 설명은 하지 않겠습니다.
+ *
+ * 새로 알게 된 사실
+ * 딱히 없습니다.
+ * */
+
 public class BJ11047 {
     private static ArrayList<Integer> money = new ArrayList<>();
     public static void main(String[] args) throws IOException {
