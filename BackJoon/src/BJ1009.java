d8109aa6902a590c05585080b71a12730f1d7a46
hwihwi99 <chamgrace@naver.com>
2022-02-25 11:16:14 +0900

    BJ1009

--- BackJoon/src/BJ1009.java
+++ BackJoon/src/BJ1009.java
@@ -4,7 +4,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BJ1009 {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
