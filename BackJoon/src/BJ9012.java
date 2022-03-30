ade781d4250a63c5964da1e3238246701f563cc9
hwihwi99 <chamgrace@naver.com>
2022-03-30 15:48:14 +0900

    백준 9012 문제풀이 성공

--- BackJoon/src/BJ9012.java
+++ BackJoon/src/BJ9012.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-
+/**
+ * 140ms
+ * */
 public class BJ9012 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
