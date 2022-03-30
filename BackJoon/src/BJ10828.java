47a8a674a46d89ffe13f45aa4cc20c75a116308d
hwihwi99 <chamgrace@naver.com>
2022-03-30 14:49:09 +0900

    백준 10828 문제풀이 성공

--- BackJoon/src/BJ10828.java
+++ BackJoon/src/BJ10828.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
-
+/**
+ * 188ms
+ * */
 public class BJ10828 {
     public static void main(String[] args) throws IOException {
 
