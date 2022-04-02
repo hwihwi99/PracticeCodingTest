bd417238a8aa440baf1b46dd029917a440f45836
hwihwi99 <chamgrace@naver.com>
2022-04-02 14:26:51 +0900

    14425 백준 문제 풀기

--- BackJoon/src/BJ14425.java
+++ BackJoon/src/BJ14425.java
@@ -5,21 +5,22 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class BJ14425 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         HashSet<String> hashSet = new HashSet<>();
-        for(int i = 0; i<M;i++){
+        for(int i = 0; i<N; i++) {
             hashSet.add(br.readLine());
         }
-        int ans = 0;
-        for(int i = 0;i<N;i++){
-            if(hashSet.contains(br.readLine())){
-                ans++;
+        int answer = 0;
+        for(int i = 0; i<M; i++) {
+            String input = br.readLine();
+            if(hashSet.contains(input)) {
+                answer++;
             }
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
