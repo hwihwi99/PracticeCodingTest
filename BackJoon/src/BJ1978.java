51dae0955322db69e31d8632ad1db1dbb24449c2
hwihwi99 <chamgrace@naver.com>
2022-04-25 22:34:26 +0900

    1978

--- BackJoon/src/BJ1978.java
+++ BackJoon/src/BJ1978.java
@@ -6,23 +6,24 @@ import java.util.StringTokenizer;
 public class BJ1978 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int num = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int isPrime;
-        int result = 0;
-        int count;
-        st = new StringTokenizer(br.readLine()," ");
-        for(int i = 0; i<num;i++){
-            isPrime = Integer.parseInt(st.nextToken());
-            count = 0;
-            for(int j = 1;j<=isPrime;j++){
-                if(isPrime % j == 0)
+        int count = 0;
+
+        for(int i = 0; i<num; i++) {
+            int N = Integer.parseInt(st.nextToken());
+            if(N == 1)
+                continue;
+            for(int j = 2; j<N;j++) {
+                if(N % j == 0)
+                    break;
+                if(j == N-1)
                     count++;
             }
-            if(count == 2)
-                result++;
+            if(N == 2)
+                count++;
         }
-        System.out.println(result);
+        System.out.println(count);
     }
 }
