a998457c5206819e6b96721434284ced294da0c0
hwihwi99 <chamgrace@naver.com>
2022-04-25 21:34:33 +0900

    2609

--- BackJoon/src/BJ2609.java
+++ BackJoon/src/BJ2609.java
@@ -1,21 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BJ2609 {
 
-    public static int gcd(int p, int q){
+    public static int gbd(int p, int q) {
         if(q == 0)
             return p;
-        return gcd(q,p%q);
+        return gbd(q,p%q);
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
 
-        int gcd = a>b?gcd(a,b):gcd(b,a);
-        int lcm = a * (b/gcd);
-
-        System.out.println(gcd);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int gbd = gbd(a,b);
+        int lcm = gbd * (a/gbd) * (b/gbd);
+        System.out.println(gbd);
         System.out.println(lcm);
     }
 }
