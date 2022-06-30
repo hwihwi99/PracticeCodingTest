de7abf6b5c434c24de38b17220a09042530e6c0e
hwihwi99 <chamgrace@naver.com>
2022-06-30 16:46:27 +0900

    BJ1110 문제풀이

--- BackJoon/src/BJ1110.java
+++ BackJoon/src/BJ1110.java
@@ -4,17 +4,17 @@ public class BJ1110 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int answer = 0;
         int temp = N;
-        while (true) {
+        int answer = 0;
 
+        while (true){
+            answer++;
             int first = temp / 10;
             int second = temp % 10;
-            int sum = first + second;
+            int result = (first+second) % 10;
 
-            temp = 10 * second + (sum%10);
-            answer ++;
-            if(N == temp) {
+            temp = second*10 + result;
+            if(temp == N){
                 break;
             }
         }
