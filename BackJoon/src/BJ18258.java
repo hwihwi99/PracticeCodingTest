103ea6e94328de4f8a9fa560aa2740513ff3dee2
hwihwi99 <chamgrace@naver.com>
2022-07-06 21:27:19 +0900

    BJ 18258 해결

--- BackJoon/src/BJ18258.java
+++ BackJoon/src/BJ18258.java
@@ -2,51 +2,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-/**
- * 1412ms
- * */
-public class BJ18258 {
+
+public class BJ18258{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringBuilder builder = new StringBuilder();
-        StringTokenizer st;
-
+        StringBuilder sb = new StringBuilder();
         Deque<Integer> queue = new ArrayDeque<>();
-
-        for(int i = 0; i<N; i++) {
+        for(int i = 0 ; i<N; i++) {
             String cmd = br.readLine();
-
             if(cmd.contains("push")) {
-                st = new StringTokenizer(cmd);
+                StringTokenizer st = new StringTokenizer(cmd," ");
                 st.nextToken();
-                int X = Integer.parseInt(st.nextToken());
-                queue.addLast(X);
-            }else if(cmd.equals("pop")) {
+                int num = Integer.parseInt(st.nextToken());
+                queue.offer(num);
+            }else if(cmd.equals("pop")){
                 if(queue.isEmpty())
-                    builder.append(-1).append("\n");
+                    sb.append(-1).append("\n");
                 else
-                    builder.append(queue.pollFirst()).append("\n");
-            } else if(cmd.equals("size")) {
-                builder.append(queue.size()).append("\n");
-            } else if(cmd.equals("empty")) {
+                    sb.append(queue.poll()).append("\n");
+            }else if(cmd.equals("size")) {
+                sb.append(queue.size()).append("\n");
+            }else if(cmd.equals("empty")) {
                 if(queue.isEmpty())
-                    builder.append(1).append("\n");
+                    sb.append(1).append("\n");
                 else
-                    builder.append(0).append("\n");
-            } else if(cmd.equals("front")) {
+                    sb.append(0).append("\n");
+            }else if(cmd.equals("front")){
                 if(queue.isEmpty())
-                    builder.append(-1).append("\n");
+                    sb.append(-1).append("\n");
                 else
-                    builder.append(queue.peekFirst()).append("\n");
-            } else if(cmd.equals("back")) {
+                    sb.append(queue.getFirst()).append("\n");
+            }else if(cmd.equals("back")) {
                 if(queue.isEmpty())
-                    builder.append(-1).append("\n");
+                    sb.append(-1).append("\n");
                 else
-                    builder.append(queue.peekLast()).append("\n");
+                    sb.append(queue.getLast()).append("\n");
             }
         }
-        builder.deleteCharAt(builder.lastIndexOf("\n"));
-        System.out.println(builder);
+        System.out.println(sb);
     }
 }
